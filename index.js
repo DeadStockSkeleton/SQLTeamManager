66cfb06f6e80f1890281b0aea92de034f3c89a07
Iyana Medlock <iyanamedlock@gmail.com>
2021-03-07 11:41:49 -0600

    adding and deleting roles finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-
+var req = "";
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -32,6 +32,7 @@ const start = () => {
         "View All Roles",
         "Add Role",
         "Remove Role",
+        "View Managers",
         "Add Manager",
         "Remove Manager",
       ],
@@ -55,6 +56,19 @@ const start = () => {
             break;
         case "Update Employee Role":
             updateEmployee();
+            break;
+        case "Update Employee Manager":
+            updateManager();
+            break;
+        case "View All Roles":
+            viewRoles();
+            break;
+        case "Add Role":
+            addRoles();
+            break;
+        case "Remove Role":
+            removeRole();
+            break;
       }
     });
 };
@@ -69,6 +83,131 @@ const viewAll = () => {
     }
   );
 };
+
+const removeRole = () => {
+    connection.query("SELECT role.title FROM role", (err, res) => {
+        if (err) throw err;
+    
+        inquirer
+          .prompt([
+            {
+              name: "roles",
+              type: "list",
+              choices() {
+                const choiceArray = [];
+                res.forEach(({ title }) => {
+                  choiceArray.push(title);
+                });
+                return choiceArray;
+              },
+              message: "Select a role",
+            },
+          ])
+          .then((answer) => {
+            connection.query(
+              `DELETE FROM role WHERE role.title = "${answer.roles}"`,
+              (err, res) => {
+                console.log('Role deleted!');
+                viewAll();
+              }
+            );
+          });
+      });
+}
+
+const addRoles = () => {
+    connection.query("SELECT department.name FROM department", (err, res) => {
+        if (err) throw err;
+    
+        inquirer
+          .prompt([
+             {
+            name: "roleName",
+            type: "input",
+            message: "New Role name?",
+            validate(value) {
+                if (value.length === 0) {
+                  return false;
+                }
+                return true;
+              },
+
+        },{
+            name: "salary",
+            type: "number",
+            message: "Salary of this role?",
+            validate(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              },
+
+        } ,
+            {
+              name: "depName",
+              type: "list",
+              choices() {
+                const choiceArray = [];
+                res.forEach(({ name }) => {
+                  choiceArray.push(name);
+                });
+                return choiceArray;
+              },
+              message: "Select a department",
+            },
+          ]).then((answer) => {
+            connection.query(
+                `SELECT * FROM department WHERE department.name = "${answer.depName}"`,
+                (err, rs) => {
+                  if (err) throw err;
+                  req = `INSERT INTO role (title, salary, department_id) value ("${answer.roleName}", ${answer.salary},${rs[0].id})`
+                  addR(req);
+                }
+              );
+        })
+      });
+    };
+    
+    const addR = (x) => {
+        connection.query(x, (err) => {
+            if (err) throw err;
+            console.log('New Role added!')
+            viewAll();
+        })
+    }
+        
+
+const viewRoles = () => {
+    connection.query("SELECT role.title FROM role", (err, res) => {
+        if (err) throw err;
+    
+        inquirer
+          .prompt([
+            {
+              name: "roles",
+              type: "list",
+              choices() {
+                const choiceArray = [];
+                res.forEach(({ title }) => {
+                  choiceArray.push(title);
+                });
+                return choiceArray;
+              },
+              message: "Select a role",
+            },
+          ])
+          .then((answer) => {
+            connection.query(
+              `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, manager.manager FROM employee INNER JOIN department ON (employee.role_id = department.id) INNER JOIN role ON (role_id = role.id) INNER JOIN manager ON (employee.manager_id = manager.id) WHERE role.title = "${answer.roles}"`,
+              (err, res) => {
+                console.table(res);
+                start();
+              }
+            );
+          });
+      });
+}
 
 const viewDep = () => {
   connection.query("SELECT department.name FROM department", (err, res) => {
@@ -100,7 +239,7 @@ const viewDep = () => {
       });
   });
 };
-var req = "";
+
 const addEmployee = () => {
   var newEmployee = [];
   connection.query("SELECT * FROM department", (err, res) => {
@@ -313,5 +452,55 @@ const update = (x) => {
         if (err) throw err;
         console.log('Updated!')
         viewAll();
+    })
+}
+
+const updateManager = () =>{
+    connection.query("SELECT employee.first_name, employee.last_name FROM employee", (err, res) => {
+        if (err) throw err;
+
+        inquirer
+      .prompt([
+        {
+          name: "employee",
+          type: "list",
+          choices() {
+            const choiceArray = [];
+            res.forEach(({ first_name }) => {
+                
+              choiceArray.push(first_name);
+            });
+            return choiceArray;
+          },
+          message: "Select an employee",
+        },
+      ])
+      .then((answer) => {
+          name = answer.employee;
+        connection.query("SELECT * FROM manager", (err, res) => {
+            if (err) throw err;
+            inquirer
+          .prompt([
+            {
+              name: "manager",
+              type: "list",
+              choices() {
+                const choiceArray = [];
+                res.forEach(({ manager }) => {
+                    
+                  choiceArray.push(manager);
+                });
+                return choiceArray;
+              },
+              message: "Select a Manager",
+            },
+          ]).then((answer) => {
+              connection.query(`SELECT id FROM manager WHERE manager.manager = "${answer.manager}"`, (err,res) => {
+                req = `UPDATE employee SET manager_id = ${res[0].id} WHERE first_name = "${name}"`
+                update(req);
+              })
+          })
+        })
+      });
     })
 }
